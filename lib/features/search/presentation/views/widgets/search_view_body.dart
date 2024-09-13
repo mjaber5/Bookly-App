3864bfb82a410d6e4fe67de0dd4d8deff61571f4
Mohammed Jaber <108561613+mjaber5@users.noreply.github.com>
2024-09-14 00:13:46 +0300

    refactor search result list view

--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:bookly/core/utlis/styles.dart';
-import 'package:bookly/features/home/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly/features/search/presentation/views/widgets/custom_search_text_feild.dart';
+import 'package:bookly/features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 
 class SearchViewBody extends StatelessWidget {
@@ -25,24 +25,6 @@ class SearchViewBody extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return const Padding(
-          padding: EdgeInsets.symmetric(vertical: 10.0),
-          child: BooksListViewItem(),
-        );
-      },
     );
   }
 }
