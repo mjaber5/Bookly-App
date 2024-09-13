108aa99758590c6e3d193be20fda1c8b0a8920b2
Mohammed Jaber <108561613+mjaber5@users.noreply.github.com>
2024-09-13 16:45:42 +0300

    refactor book details view

---
+++ lib/features/home/presentation/views/widgets/books_details_section.dart
@@ -0,0 +1,47 @@
+import 'package:bookly/core/utlis/styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
+import 'package:bookly/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly/features/home/presentation/views/widgets/custom_book_item.dart';
+import 'package:flutter/material.dart';
+
+class BooksDetailsSection extends StatelessWidget {
+  const BooksDetailsSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 24,
+            horizontal: width * 0.11,
+          ),
+          child: const CustomBookImage(),
+        ),
+        const SizedBox(height: 43),
+        const Text(
+          'The Jungle Book',
+          style: Styles.textStyle30,
+        ),
+        const SizedBox(height: 6),
+        Opacity(
+          opacity: 0.7,
+          child: Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18.copyWith(
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        const SizedBox(height: 18),
+        const BookRating(
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
+        const SizedBox(height: 37),
+        const BooksAction(),
+      ],
+    );
+  }
+}
