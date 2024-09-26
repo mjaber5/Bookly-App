3c77020bfc889629b592e420be15382d285cba9e
Mohammed Jaber <108561613+mjaber5@users.noreply.github.com>
2024-09-26 22:40:23 +0300

    newest books cubit done

--- lib/features/home/presentation/view_model/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/view_model/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/data/models/book_models/book_model.dart';
+import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold(
+      (failure) {
+        emit(NewsetBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewsetBooksSuccess(books));
+      },
+    );
+  }
 }
