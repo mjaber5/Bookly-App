2164fe15235fc8c72ce760936da72734ae1c4c56
Mohammed Jaber <108561613+mjaber5@users.noreply.github.com>
2024-09-29 20:20:40 +0300

    fix issue with loading data

--- lib/features/home/presentation/view_model/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/view_model/newset_books_cubit/newset_books_cubit.dart
@@ -1,7 +1,7 @@
-import 'package:bloc/bloc.dart';
 import 'package:bookly/features/home/data/models/book_models/book_model.dart';
 import 'package:bookly/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'newset_books_state.dart';
 
@@ -11,7 +11,7 @@ class NewsetBooksCubit extends Cubit<NewsetBooksState> {
 
   Future<void> fetchNewestBooks() async {
     emit(NewsetBooksLoading());
-    var result = await homeRepo.fetchFeaturedBooks();
+    var result = await homeRepo.fetchNewsetBooks();
 
     result.fold(
       (failure) {
